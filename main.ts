e6443425e430521f7db644356770be738a320542
MurphyMeadowhead <77684838+MurphyMeadowhead@users.noreply.github.com>
2021-01-20 12:13:52 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
 input.onButtonPressed(Button.A, function () {
-    Countdown_number = 2
-    basic.showNumber(Countdown_number)
-    while (Countdown_number > 0) {
-        for (let index = 0; index < 60; index++) {
-            basic.pause(1000)
-        }
-        Countdown_number += -1
-    }
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    Countdown_number = 30
 })
 input.onButtonPressed(Button.B, function () {
     Countdown_number = -1
@@ -15,18 +15,15 @@ let Countdown_number = 0
 Countdown_number = -1
 basic.forever(function () {
     while (Countdown_number == -1) {
-        basic.showString("HELLO")
-        basic.pause(500)
         basic.showIcon(IconNames.Happy)
         basic.clearScreen()
-        basic.pause(500)
-        basic.showString("30 MINUTE TIMER")
         basic.pause(500)
     }
 })
 basic.forever(function () {
     while (Countdown_number > 1) {
         basic.showNumber(Countdown_number)
+        basic.showString("MINS")
     }
 })
 basic.forever(function () {
@@ -44,5 +41,13 @@ basic.forever(function () {
         basic.pause(500)
         basic.showString("STAND BY")
         basic.pause(500)
+    }
+})
+basic.forever(function () {
+    while (Countdown_number > 0) {
+        for (let index = 0; index < 60; index++) {
+            basic.pause(1000)
+        }
+        Countdown_number += -1
     }
 })
