3adc23ccd88450ef5d585e15d99182be7baf2101
MurphyMeadowhead <77684838+MurphyMeadowhead@users.noreply.github.com>
2021-01-25 13:31:05 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 input.onButtonPressed(Button.A, function () {
+    led.setBrightness(255)
     basic.showLeds(`
         # # # # #
         # # # # #
@@ -9,35 +10,15 @@ input.onButtonPressed(Button.A, function () {
     Countdown_number = 1
 })
 input.onButtonPressed(Button.B, function () {
+    led.setBrightness(255)
     Countdown_number = -1
 })
 let Countdown_number = 0
+led.setBrightness(255)
 Countdown_number = -1
 basic.forever(function () {
-    while (Countdown_number > 0 && Countdown_number < 30) {
-        basic.showNumber(Countdown_number)
-        basic.pause(1000)
-        basic.clearScreen()
-        basic.pause(200)
-    }
-})
-basic.forever(function () {
-    while (Countdown_number == -1) {
-        basic.showIcon(IconNames.Happy)
-        basic.clearScreen()
-        basic.pause(500)
-    }
-})
-basic.forever(function () {
-    while (Countdown_number > 30) {
-        basic.showIcon(IconNames.No)
-        basic.pause(1000)
-        basic.showIcon(IconNames.SmallDiamond)
-        basic.pause(1000)
-    }
-})
-basic.forever(function () {
     while (Countdown_number == 30) {
+        led.setBrightness(255)
         basic.showNumber(Countdown_number)
         basic.pause(500)
         basic.showString("FINAL MINUTE")
@@ -54,5 +35,31 @@ basic.forever(function () {
             basic.pause(1000)
         }
         Countdown_number += 1
+    }
+})
+basic.forever(function () {
+    while (Countdown_number > 0 && Countdown_number < 30) {
+        led.setBrightness(255)
+        basic.showNumber(Countdown_number)
+        basic.pause(1000)
+        led.fadeOut(1000)
+basic.pause(200)
+    }
+})
+basic.forever(function () {
+    while (Countdown_number == -1) {
+        led.setBrightness(255)
+        basic.showIcon(IconNames.Happy)
+        basic.clearScreen()
+        basic.pause(500)
+    }
+})
+basic.forever(function () {
+    while (Countdown_number > 30) {
+        led.setBrightness(255)
+        basic.showIcon(IconNames.No)
+        basic.pause(1000)
+        basic.showIcon(IconNames.SmallDiamond)
+        basic.pause(1000)
     }
 })
